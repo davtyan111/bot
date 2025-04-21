30b2a3203b38c071a26fec740001a8e9d676a207
davtyan111 <davtyan.rafayel.37@gmail.com>
2025-04-21 19:40:07 +0400

    fixed binance issue

--- sources/binance.py
+++ sources/binance.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bot.config import CHROMEDRIVER_PATH, SOURCE_USDT_BINANCE
+from bot.config import CHROMEDRIVER_PATH, SOURCE_USDT_BINANCE ,SOURCE_BINANCE_BTC_USDT
 
 def get_usdt_price_Binance():
     options = webdriver.ChromeOptions()
@@ -30,15 +30,20 @@ def get_btc_price_Binance():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     service = Service(CHROMEDRIVER_PATH)
+
     driver = webdriver.Chrome(service=service, options=options)
-    driver.get(SOURCE_USDT_BINANCE)
+    driver.get(SOURCE_BINANCE_BTC_USDT)
 
     try:
         price_element = WebDriverWait(driver, 15).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "div.lock-ltr.text-subtitle1.font-medium"))
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "div.lock-ltr.text-subtitle1.font-medium")
+            )
         )
-        price = f"1 BTC ≈ {price_element.text} USDT"
+        price = price_element.text.strip()
+        price = f"1 BTC ≈ {price} USDT"
     except Exception as e:
         price = f"Error: {e}"
     driver.quit()
     return price
+
